785e2372197c8b75ed934bc6d7e5e341beed236c
Lucas Willian <lucas.willianxd@gmail.com>
2025-10-22 21:34:47 -0300

    feat(signup): finaliza página de cadastro

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router";
+
+import { api } from "../services/api";
+
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
-import { useState } from "react";
+
+const singUpSchema = z
+  .object({
+    name: z.string().trim().min(2, { message: "Informe o seu nome." }),
+    email: z.email({ message: "O e-mail informado é inválido." }),
+    password: z
+      .string()
+      .min(6, { message: "Sua senha deve ter pelo menos 6 dígitos." }),
+    passwordConfirm: z.string({ message: "Confirme sua senha." }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas devem ser iguais.",
+    path: ["passwordConfirm"],
+  });
 
 export function SignUp() {
   const [name, setName] = useState("");
@@ -9,11 +29,43 @@ export function SignUp() {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
+  const navigate = useNavigate();
+
+  async function onSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    console.log(name, email, password, passwordConfirm);
+    try {
+      setIsLoading(true);
+
+      const data = singUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+
+      await api.post("/users", data);
+
+      if (confirm("Cadastrado com sucesso. Deseja ir para a tela de entrar?")) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Não foi possível cadastrar.");
+    } finally {
+      setIsLoading(false);
+    }
   }
+
   return (
     <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
